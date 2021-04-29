c251be9239f1664b98c28c07a88dfbdf14bd4ef1
galymax <werkhowski@gmail.com>
2021-04-29 14:32:43 +0200

    mongoose filtering

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,12 +1,18 @@
 const Record = require('../models/Record')
 
 // get all records
+// http://localhost:3000/records/?pageNumber=0&recordsPerPage=6&sortField=price&sortOrder=-1&searchField=title&search=
 exports.getRecords = (req, res, next) => {
+    const { pageNumber, recordsPerPage, sortOrder, sortField, searchField, search } = req.query
     // access db
-    Record.find((err, records) => {
+    Record.find({[searchField]: {$regex: search, $options: 'i'}}, (err, records) => {
       if (err) return console.error(err);
       res.json(records)
     })
+    .limit(Number(recordsPerPage))
+    // start from apecific entry number
+    .skip(pageNumber * recordsPerPage)
+    .sort({[sortField]: sortOrder})
 }
 
 // get specific record
